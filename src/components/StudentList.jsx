d4f0e1a87a94d140d148dec9400898c6868568b4
Jenny Massari <jennymartins13@gmail.com>
2024-11-28 20:30:53 -0800

    Added styling component.

--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -1,16 +1,19 @@
-// import './StudentList.css';
+import './StudentList.css';
 
 const StudentList = () => {
-  return (
-    <section>
-      <h2 className="student-list__heading">Student List</h2>
-      <ul className="student-list">
-        <li>Student A</li>
-        <li>Student B</li>
-        <li>Student C</li>
-      </ul>
-    </section>
-  );
+    const headingClass = 'student-list__heading';
+    const listClass = 'student-list';
+
+    return (
+        <section>
+            <h2 className={headingClass}>Student List</h2>
+            <ul className={listClass}>
+                <li>Student A</li>
+                <li>Student B</li>
+                <li>Student C</li>
+            </ul>
+        </section>
+    );
 };
 
 export default StudentList;
